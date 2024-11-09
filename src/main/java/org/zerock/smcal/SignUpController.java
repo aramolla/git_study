0da65540129543bf0a235ad98e78fdbda826e8ab
aramolla <cayeong092@gmail.com>
2024-11-10 02:57:13 +0900

    signup_to_main

--- src/main/java/org/zerock/smcal/SignUpController.java
+++ src/main/java/org/zerock/smcal/SignUpController.java
@@ -6,6 +6,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.zerock.smcal.util.Database;
 
@@ -18,7 +19,11 @@ public class SignUpController extends HttpServlet {
 
         // Database의 addUser 메서드를 호출
         if (Database.addUser(username, password)) {
-            response.sendRedirect("login");
+            // 회원가입 성공 시, 세션 생성 및 로그인 상태 설정
+            HttpSession session = request.getSession();
+            session.setAttribute("user", username);
+
+            response.sendRedirect("smcal");
         } else {
             response.sendRedirect("signupError");
         }
